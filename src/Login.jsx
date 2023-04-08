d3cafa1d82983452ec5ff3608f1974a217288172
vijayakumar58 <vijayalingam58@gmail.com>
2023-04-08 19:10:36 +0530

    add

--- src/Login.jsx
+++ src/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
                   <div className="col-lg-6">
                     <div className="p-5">
                       <div className="text-center">
-                        <h1 className="h4 text-gray-900 mb-4">Welcome Crud!</h1>
+                        <h1 className="h4 text-gray-900 mb-4">Welcome OIP FullStack Deployment!</h1>
                       </div>
                       <form className="user">
                         <div className="form-group">
